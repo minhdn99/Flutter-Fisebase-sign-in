5d7d668eb7661f3af23caef4182618714919cd09
minhdn99 <minhah6dn@gmail.com>
2021-06-02 15:52:10 +0700

    first commit

---
+++ lib/HomeDemo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeDemo extends StatefulWidget {
+  @override
+  _HomeDemoState createState() => _HomeDemoState();
+}
+class _HomeDemoState extends State<HomeDemo> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
+  }
+}
